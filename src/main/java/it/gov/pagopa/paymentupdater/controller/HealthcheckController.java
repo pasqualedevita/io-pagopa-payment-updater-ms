2bd59e599bc4e17a5cd452303e58f3ae0b868190
AleDore <alessio85.dore@gmail.com>
2022-06-14 12:02:56 +0200

    add healthcheck controller

---
+++ src/main/java/it/gov/pagopa/paymentupdater/controller/HealthcheckController.java
@@ -0,0 +1,35 @@
+package it.gov.pagopa.paymentupdater.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.swagger.annotations.Api;
+
+@Api(tags = "API  HealthCheck")
+@RestController
+@Validated
+@RequestMapping(value = "api/v1/health", produces = APPLICATION_JSON_VALUE, consumes = {
+        MediaType.APPLICATION_JSON_VALUE, MediaType.ALL_VALUE }, method = RequestMethod.OPTIONS)
+public class HealthcheckController {
+
+    @GetMapping(value = "/ready")
+    public ResponseEntity<Object> checkReady() throws JsonProcessingException {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/live")
+    public ResponseEntity<Object> checkLive() throws JsonProcessingException {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
