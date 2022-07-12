4aaa6e6b4a3a5e5eda7995f6d73c4503905174c7
AleDore <alessio85.dore@gmail.com>
2022-07-12 17:30:44 +0200

    add enable retry

--- src/main/java/it/gov/pagopa/paymentupdater/Application.java
+++ src/main/java/it/gov/pagopa/paymentupdater/Application.java
@@ -2,11 +2,13 @@ package it.gov.pagopa.paymentupdater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
-public class Application{
-	
+public class Application {
+
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
